51cc95f467da10c8f87b1d7314d3ed072b750d32
Marko Dobrijevic <dobrijevic.sr73.2021@uns.ac.rs>
2023-06-26 17:06:32 +0200

    Navigation Bar test

--- app/src/main/java/com/example/slagalica/HomeActivity.java
+++ app/src/main/java/com/example/slagalica/HomeActivity.java
@@ -1,14 +1,72 @@
 package com.example.slagalica;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        // Postavljanje toggle dugmeta za Navigation Drawer
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, android.R.string.ok, android.R.string.ok);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Obrada klikova na stavke navigacije
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.menu_profile) {
+                    // Logika za otvaranje profila
+                    openProfile();
+                } else if (itemId == R.id.menu_leaderboard) {
+                    // Logika za otvaranje rang liste
+                    openLeaderboard();
+                } else if (itemId == R.id.menu_logout) {
+                    // Logika za odjavu
+                    logout();
+                }
+
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    private void openProfile() {
+        // Implementacija otvaranja profila
+        Intent homeIntent = new Intent(HomeActivity.this, ProfileActivity.class);
+        startActivity(homeIntent);
+        finish();
+    }
+
+    private void openLeaderboard() {
+        // Implementacija otvaranja rang liste
+    }
+
+    private void logout() {
+        // Implementacija odjave
     }
 }
