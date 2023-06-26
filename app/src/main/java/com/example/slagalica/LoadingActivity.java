be5f5188333038cfbba9dbbba2dc706375fc3432
Milos4 <miso.27@hotmail.com>
2023-06-26 15:24:10 +0200

    First commit

---
+++ app/src/main/java/com/example/slagalica/LoadingActivity.java
@@ -0,0 +1,27 @@
+package com.example.slagalica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(LoadingActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
