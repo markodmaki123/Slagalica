1009c4b7a07727278dac8b800ed399860ad6f6e7
Milos4 <miso.27@hotmail.com>
2023-06-28 23:55:43 +0200

    Uradnjena logika u moj broj i uspostacvljenja veza da se pokrene moj borj na ova telefona

--- app/src/main/java/com/example/slagalica/HomeActivity.java
+++ app/src/main/java/com/example/slagalica/HomeActivity.java
@@ -5,30 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
+import com.example.slagalica.games.MojBrojActivity;
 import com.google.android.material.navigation.NavigationView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class HomeActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
     private NavigationView navigationView;
+    private Button btnZapocni;
+
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private BufferedReader input;
+    private PrintWriter output;
+    private static final int DELAY_CHECK_HOST = 5000; // 5000 milisekundi (5 sekundi)
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         drawerLayout = findViewById(R.id.drawerLayout);
         navigationView = findViewById(R.id.navigationView);
+        btnZapocni = findViewById(R.id.BTNzapocniIgru);
 
         // Postavljanje toggle dugmeta za Navigation Drawer
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, android.R.string.ok, android.R.string.ok);
@@ -56,7 +82,31 @@ public class HomeActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        btnZapocni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Pozivanje metoda za uspostavljanje veze
+                // Pozivanje metode isHost() nakon 5 sekundi
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        boolean isHost = isHost();
+                        if (isHost) {
+                            connectToServer();
+                        } else {
+                            startServer();
+
+                        }
+                    }
+                }, DELAY_CHECK_HOST);
+
+                //    Intent intent = new Intent(HomeActivity.this, MojBrojActivity.class);
+               //     startActivity(intent);
+            }
+        });
     }
+
 
     private void openProfile() {
         // Implementacija otvaranja profila
@@ -73,5 +123,137 @@ public class HomeActivity extends AppCompatActivity {
         Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private boolean isHost() {
+        try {
+            final int timeout = 5000; // 5000 milisekundi (5 sekundi)
+            final int port = 1234; // Odaberite prikladni port
+
+            AsyncTask<Void, Void, Boolean> task = new AsyncTask<Void, Void, Boolean>() {
+                @SuppressLint("StaticFieldLeak")
+                @Override
+                protected Boolean doInBackground(Void... voids) {
+                    try {
+                        Socket testSocket = new Socket();
+                        testSocket.connect(new InetSocketAddress("192.168.0.12", port), timeout); // Zamijenite sa stvarnom IP adresom domaćina (servera)
+                        testSocket.close();
+                        return true;
+                    } catch (IOException e) {
+                        return false;
+                    }
+                }
+            };
+            return task.execute().get(); // Pokreće se asinkroni zadatak i čeka se rezultat
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private void startServer() {
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                try {
+                    serverSocket = new ServerSocket(1234); // Odaberite prikladni port
+                    clientSocket = serverSocket.accept();
+                    input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    output = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                    // Primanje i slanje poruka
+
+                    return null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        startGameActivity(true); // Pokreni novu aktivnost kao domaćin (server)
+                    }
+                });
+            }
+        };
+
+        task.execute(); // Pokreće se asinkroni zadatak
+    }
+
+    private void connectToServer() {
+        AsyncTask<Void, Void, Boolean> task = new AsyncTask<Void, Void, Boolean>() {
+            @Override
+            protected Boolean doInBackground(Void... voids) {
+                try {
+                    String serverIp = "192.168.0.12"; // Zamijenite s IP adresom domaćina (servera)
+                    int port = 1234; // Zamijenite s odgovarajućim portom
+
+                    clientSocket = new Socket(serverIp, port);
+
+                    input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    output = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                    return true;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Boolean isConnected) {
+                if (isConnected) {
+                    startGameActivity(false); // Pokreni novu aktivnost kao klijent
+                } else {
+                    // Neuspjeh pri povezivanju s serverom
+                    // Ovdje možete dodati odgovarajuće postupke u slučaju neuspjeha
+                }
+            }
+        };
+
+        task.execute();
+    }
+
+    private void sendMessage(String message) {
+        output.println(message);
+    }
+
+    // Ostatak koda vaše aktivnosti...
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+
+            if (clientSocket != null) {
+                clientSocket.close();
+            }
+
+            if (input != null) {
+                input.close();
+            }
+
+            if (output != null) {
+                output.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Uklanjanje odgođenog zadatka ako je aktivnost uništena prije isteka vremena
+        handler.removeCallbacksAndMessages(null);
+    }
+    private void startGameActivity(boolean isHost) {
+        Intent intent = new Intent(HomeActivity.this, MojBrojActivity.class);
+        intent.putExtra("isHost", isHost);
+        startActivity(intent);
+        finish(); // Zatvorite trenutnu aktivnost ako više nije potrebna
     }
 }
--- app/src/main/java/com/example/slagalica/games/MojBrojActivity.java
+++ app/src/main/java/com/example/slagalica/games/MojBrojActivity.java
@@ -3,14 +3,179 @@ package com.example.slagalica.games;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
 import com.example.slagalica.R;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MojBrojActivity extends AppCompatActivity {
+
+    private TextView tvTrazeniBroj;
+    private TextView tvJednocifreni1;
+    private TextView tvJednocifreni2;
+    private TextView tvJednocifreni3;
+    private TextView tvJednocifreni4;
+    private TextView tvSrednji;
+    private TextView tvVeliki;
+    private Button btnStop;
+    private Button btnStart;
+    private EditText etResenje;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moj_broj);
+
+
+
+        tvTrazeniBroj = findViewById(R.id.TVTrazeniBroj);
+        tvJednocifreni1 = findViewById(R.id.TVJednocifreni1);
+        tvJednocifreni2 = findViewById(R.id.TVJednocifreni2);
+        tvJednocifreni3 = findViewById(R.id.TVJednocifreni3);
+        tvJednocifreni4 = findViewById(R.id.TVJednocifreni4);
+        tvSrednji = findViewById(R.id.TVSrednji);
+        tvVeliki = findViewById(R.id.TVVeliki);
+        btnStop = findViewById(R.id.BTNStop);
+        btnStart = findViewById(R.id.BTNStart);
+        etResenje = findViewById(R.id.ETResenje);
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generisiBrojeve();
+            }
+        });
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                proveriResenje();
+            }
+        });
+        int trazeniBroj = generisiSlucajniBroj();
+        tvTrazeniBroj.setText(String.valueOf(trazeniBroj));
+    }
+
+    private int generisiSlucajniBroj() {
+        // Generisanje slučajnog broja od 1 do 100
+        Random random = new Random();
+        return random.nextInt(999) + 1;
+    }
+
+    private void generisiBrojeve() {
+        Random random = new Random();
+
+        // Generisanje prvih četiri jednocifrena broja
+        int jednocifreni1 = random.nextInt(9) + 1;
+        int jednocifreni2 = random.nextInt(9) + 1;
+        int jednocifreni3 = random.nextInt(9) + 1;
+        int jednocifreni4 = random.nextInt(9) + 1;
+
+        // Generisanje srednjeg broja (10, 15 ili 20)
+        int srednji;
+        int srednjiIndex = random.nextInt(3);
+        if (srednjiIndex == 0) {
+            srednji = 10;
+        } else if (srednjiIndex == 1) {
+            srednji = 15;
+        } else {
+            srednji = 20;
+        }
+
+        // Generisanje zadnjeg broja (25, 50, 75 ili 100)
+        int zadnjiIndex = random.nextInt(4);
+        int zadnji;
+        switch (zadnjiIndex) {
+            case 0:
+                zadnji = 25;
+                break;
+            case 1:
+                zadnji = 50;
+                break;
+            case 2:
+                zadnji = 75;
+                break;
+            default:
+                zadnji = 100;
+                break;
+        }
+
+        // Postavljanje generisanih brojeva u odgovarajuća TextView polja
+        tvJednocifreni1.setText(String.valueOf(jednocifreni1));
+        tvJednocifreni2.setText(String.valueOf(jednocifreni2));
+        tvJednocifreni3.setText(String.valueOf(jednocifreni3));
+        tvJednocifreni4.setText(String.valueOf(jednocifreni4));
+        tvSrednji.setText(String.valueOf(srednji));
+        tvVeliki.setText(String.valueOf(zadnji));
+    }
+
+    private int izracunajKompleksniIzraz(String izraz) {
+
+        try {
+            Expression expression = new ExpressionBuilder(izraz).build();
+            double result = expression.evaluate();
+            return (int) result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    private void proveriResenje() {
+        // Provjeri da li su svi uneseni brojevi prisutni u ponuđenim brojevima
+
+            String unesenoResenje = etResenje.getText().toString();
+            int trazeniBroj = Integer.parseInt(tvTrazeniBroj.getText().toString());
+
+            // Provera tačnosti rešenja
+            int rezultat = izracunajKompleksniIzraz(unesenoResenje);
+            boolean tacno = rezultat == trazeniBroj;
+
+        // Provera da li su brojevi u rešenju prisutni u ponuđenim brojevima
+        List<Integer> ponudjeniBrojevi = new ArrayList<>();
+        ponudjeniBrojevi.add(Integer.parseInt(tvJednocifreni1.getText().toString()));
+        ponudjeniBrojevi.add(Integer.parseInt(tvJednocifreni2.getText().toString()));
+        ponudjeniBrojevi.add(Integer.parseInt(tvJednocifreni3.getText().toString()));
+        ponudjeniBrojevi.add(Integer.parseInt(tvJednocifreni4.getText().toString()));
+        ponudjeniBrojevi.add(Integer.parseInt(tvSrednji.getText().toString()));
+        ponudjeniBrojevi.add(Integer.parseInt(tvVeliki.getText().toString()));
+
+        boolean brojeviPostoje = true;
+        String[] brojeviIzraza = unesenoResenje.split("[+\\-*/()]");
+        for (String brojStr : brojeviIzraza) {
+            if (!brojStr.isEmpty()) {
+                int broj = Integer.parseInt(brojStr);
+                if (!ponudjeniBrojevi.contains(broj)) {
+                    brojeviPostoje = false;
+                    break;
+                }
+            }
+        }
+
+        // Prikazivanje rezultata korisniku
+        if (tacno && brojeviPostoje) {
+            Toast.makeText(this, "Čestitamo! Rešenje je tačno!", Toast.LENGTH_SHORT).show();
+        } else if (!tacno) {
+            Toast.makeText(this, "Rešenje nije tačno. Pokušaj ponovo.", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Neki brojevi u rešenju ne postoje među ponuđenim brojevima.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
